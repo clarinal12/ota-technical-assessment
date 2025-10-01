73c59edca2fedf4ca9cbb078111e55b3d4b5661e
Al Sagarino <al@mazer.co.uk>
2025-10-01 23:27:37 +0800

    chore: remove logs

--- app/gm/[username]/page.tsx
+++ app/gm/[username]/page.tsx
@@ -22,8 +22,6 @@ export default async function GrandMaster({
   try {
     const data = await fetch(`https://api.chess.com/pub/player/${username}`);
     gm = await data.json();
-
-    console.log({ data, gm });
   } catch (err) {
     error = "Could not load player data.";
   }
